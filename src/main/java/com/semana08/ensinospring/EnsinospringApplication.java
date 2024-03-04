b47ab5989f54abb1c6f03367b9949c886edb32a3
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-03-04 17:39:11 -0300

    ex01

---
+++ src/main/java/com/semana08/ensinospring/EnsinospringApplication.java
@@ -0,0 +1,13 @@
+package com.semana08.ensinospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsinospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsinospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semana08/ensinospring/EnsinospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semana08.ensinospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsinospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
