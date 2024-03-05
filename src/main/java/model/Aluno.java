acf985a3fa592bdd367eaca98bf91d5e84bd3609
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-03-05 13:57:41 -0300

    Ex04 - atualização Classe aluno

--- src/main/java/model/Aluno.java
+++ src/main/java/model/Aluno.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Getter
 public class Aluno {
     private static Integer idAluno = 1;
+    private static List<Aluno> alunos = new ArrayList<>();
     @Setter
     private String nomeAluno;
     @Setter
@@ -24,4 +27,28 @@ public class Aluno {
         this.dataNascimento = dataNascimento;
     }
 
+    public static List<Aluno> getAluno() {
+        List<Aluno> alunos = null;
+        return alunos;
+    }
+
+
+    public static Aluno buscarPorId(Integer id) throws Exception {
+        for (Aluno aluno : alunos) {
+            if (aluno.getIdAluno().equals(id)) {
+                return aluno;
+            }
+        }
+        throw new Exception("O Aluno não foi encontrado!");
+    }
+
+    public static Aluno cadastrarAluno(Aluno aluno) {
+        aluno.idAluno = obterProximoId();
+        alunos.add(aluno);
+        return aluno;
+    }
+
+    private Object getIdAluno() {
+        return null;
+    }
 }
